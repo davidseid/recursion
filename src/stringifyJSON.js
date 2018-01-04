329569caaedc26e2db56cc088398e20569782065
David Seidenberg <davidseidenberg@gmail.com>
2018-01-04 11:43:54 -0800

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  
+  if (obj === null) {
+    return 'null';
+  }
+  
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  
+  if (Array.isArray(obj)) {
+    var answer = '[';
+    
+    for (var i = 0; i < obj.length; i++) {
+      answer += stringifyJSON(obj[i]);
+      answer += ',';
+    }
+    if (answer[answer.length - 1] === ',') {
+      answer = answer.slice(0, answer.length - 1);
+    }
+    return answer + ']';
+  }
+  
+  if (typeof obj === 'object') {
+    var answer = '{';
+    for (var propKey in obj) {
+    
+      if (typeof obj[propKey] === 'function') {
+      } else if (obj[propKey] === undefined) {
+      } else {
+      
+    
+		  answer += stringifyJSON(propKey);
+		  answer += ':';
+		  answer += stringifyJSON(obj[propKey]);
+		  answer += ',';
+	  }
+    }
+    
+    if (answer[answer.length - 1] === ',') {
+      answer = answer.slice(0, answer.length - 1);
+    }
+    
+    return answer += '}';
+  }
 };
